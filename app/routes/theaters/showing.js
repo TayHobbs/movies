1f46efd386e1f91d1ded6f820cf674d5c5533b0d
Taylor Hobbs <taylor@feemon.com>
2015-05-05 07:09:26 -0500

    Refactor: Routes are now Javascript

---
+++ app/routes/theaters/showing.js
@@ -0,0 +1,22 @@
+import Ember from 'ember';
+import Ajax from '../../mixins/ajax';
+
+export default Ember.Route.extend(Ajax, {
+  model() {
+    Ember.RSVP.all([
+      this._ajax({
+        type: 'GET',
+        url: 'http://api.rottentomatoes.com/api/public/v1.0/lists/movies/in_theaters.json?apikey=',
+        dataType: 'jsonp',
+        done: function(data) {
+          return data;
+        },
+        fail: function() {
+          console.log("It does not do well to dwell on failure");
+        }
+      })
+    ]).then(function(results) {
+      return { movies: results[0]['movies'] };
+    });
+  }
+});
